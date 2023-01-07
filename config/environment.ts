330e7d00b9c9625523e7f5db5be2caf80ad1363b
NextDev <nextdevelopment1111@gmail.com>
2023-01-07 12:11:47 +0530

    Depoly environment fetcher file

---
+++ config/environment.ts
@@ -0,0 +1,16 @@
+// loading environment variables
+const supabaseUrl : string | undefined = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey : string | undefined = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if(supabaseKey == undefined || supabaseUrl == undefined){
+    throw new Error('Supabase config not loaded')
+}
+
+// creating an object for exporting environment variables
+const environmentVariables = {
+    supabaseKey,
+    supabaseUrl
+}
+
+// export the object 
+export default environmentVariables
